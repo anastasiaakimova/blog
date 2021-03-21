4141d6c1e544364b3b45ba2af024868853e26ad8
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-03-21 21:40:20 +0300

    add security

---
+++ src/main/java/com/akimova/web/blog/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.akimova.web.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//содержит конфигурацию веб-слоя
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
